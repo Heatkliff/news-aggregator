4ee1167cd632959d95e003253514bea9353e7355
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2025-04-05 21:41:55 +0300

    Added admin register for Source, Category, News, Tag

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Source, Category, News, Tag
 
-# Register your models here.
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'rss_url', 'needs_scraping', 'active')
+    list_filter = ('active', 'needs_scraping')
+    search_fields = ('name', 'url')
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'source', 'created_at')
+    list_filter = ('source', 'categories')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_at'
+    filter_horizontal = ('categories',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    filter_horizontal = ('news',)
