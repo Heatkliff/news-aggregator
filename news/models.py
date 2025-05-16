2140b427475d67c469d04effc78aaac13a0da09b
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2025-05-16 20:54:31 +0300

    Move custom slugify function to utils file; define BaseModel with common methods for all models: get max_length for a specified field, truncate data to this value, generate safe slug; add slug for SiteCategory model

--- news/models.py
+++ news/models.py
@@ -1,35 +1,53 @@
 import uuid
 
 from django.db import models
-from django.utils.text import slugify as django_slugify
+from .utils import slugify
 
 
-def slugify(text):
+class BaseModel(models.Model):
     """
-    Custom slugify function that handles cyrillic characters by transliterating them to latin
+    Abstract base model with common methods for all models
     """
-    # Transliteration mapping
-    cyrillic_mapping = {
-        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'ґ': 'g', 'д': 'd', 'е': 'e',
-        'є': 'ye', 'ж': 'zh', 'з': 'z', 'и': 'y', 'і': 'i', 'ї': 'yi', 'й': 'y',
-        'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
-        'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch',
-        'ш': 'sh', 'щ': 'shch', 'ь': '', 'ю': 'yu', 'я': 'ya',
-        'э': 'e', 'ы': 'y', 'ъ': '', 'ё': 'yo',
-    }
+    class Meta:
+        abstract = True
+        
+    @classmethod
+    def get_field_max_length(cls, field_name):
+        """
+        Get max_length value for a specified field
+        """
+        return cls._meta.get_field(field_name).max_length
 
-    # Convert text to lowercase
-    text = text.lower()
+    @classmethod
+    def truncate_for_field(cls, value, field_name):
+        """
+        Truncate value according to field's max_length
+        """
+        if not value:
+            return value
+            
+        max_length = cls.get_field_max_length(field_name)
+        if len(value) > max_length:
+            return value[:max_length]
+        return value
+        
+    @classmethod
+    def get_safe_slug(cls, text, slug_field='slug'):
+        """
+        Generate safe slug according to field's max_length
+        """
+        if not text:
+            return ""
+            
+        base_slug = slugify(text)
+        max_length = cls.get_field_max_length(slug_field)
+        
+        if len(base_slug) > max_length:
+            return base_slug[:max_length]
+        return base_slug
 
-    # Replace Cyrillic characters with their Latin equivalents
-    for cyrillic, latin in cyrillic_mapping.items():
-        text = text.replace(cyrillic, latin)
 
-    # Use Django's slugify for the rest of the processing
-    return django_slugify(text)
-
-
-class Source(models.Model):
+class Source(BaseModel):
     """
     Model representing a news source (website/publisher)
     """
@@ -44,7 +62,7 @@ class Source(models.Model):
         return self.name
 
 
-class Category(models.Model):
+class Category(BaseModel):
     """
     Model representing a news category
     """
@@ -59,24 +77,17 @@ class Category(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            # Generate slug with custom slugify function with Cyrillic support
-            base_slug = slugify(self.name)
-
-            # Ensure slug is not too long - get max_length from the field itself
-            max_length = self._meta.get_field('slug').max_length
-            if len(base_slug) > max_length:
-                base_slug = base_slug[:max_length]
-
-            self.slug = base_slug
+            self.slug = self.get_safe_slug(self.name)
 
         super().save(*args, **kwargs)
 
 
-class SiteCategory(models.Model):
+class SiteCategory(BaseModel):
     """
     Model representing an original news category from source
     """
     name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True,
                                  related_name="site_categories")
 
@@ -86,9 +97,15 @@ class SiteCategory(models.Model):
 
     def __str__(self):
         return self.name
+        
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.get_safe_slug(self.name)
+
+        super().save(*args, **kwargs)
 
 
-class News(models.Model):
+class News(BaseModel):
     """
     Model representing a news article
     """
@@ -107,25 +124,31 @@ class News(models.Model):
         verbose_name_plural = "News"
 
     def save(self, *args, **kwargs):
+        # Ensure fields are within limits
+        if self.title:
+            self.title = self.truncate_for_field(self.title, 'title')
+            
+        if self.content:
+            self.content = self.truncate_for_field(self.content, 'content')
+            
+        if self.url:
+            self.url = self.truncate_for_field(self.url, 'url')
+        
         if not self.slug:
-            # Generate slug with custom slugify function with Cyrillic support
-            base_slug = slugify(self.title)
+            base_slug = self.get_safe_slug(self.title)
 
             # If slug is empty after slugify, use UUID
             if not base_slug:
                 base_slug = f"news-{str(uuid.uuid4())[:8]}"
-
-            # Ensure slug is not too long - get max_length from the field itself
-            max_length = self._meta.get_field('slug').max_length
-            if len(base_slug) > max_length:
-                base_slug = base_slug[:max_length]
-
-            self.slug = base_slug
+                # Make sure it's within limits
+                self.slug = self.truncate_for_field(base_slug, 'slug')
+            else:
+                self.slug = base_slug
 
         super().save(*args, **kwargs)
 
 
-class Tag(models.Model):
+class Tag(BaseModel):
     """
     Model representing a tag for news article
     """
@@ -137,15 +160,10 @@ class Tag(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
+        if self.name:
+            self.name = self.truncate_for_field(self.name, 'name')
+            
         if not self.slug:
-            # Generate slug with custom slugify function with Cyrillic support
-            base_slug = slugify(self.name)
-
-            # Ensure slug is not too long - get max_length from the field itself
-            max_length = self._meta.get_field('slug').max_length
-            if len(base_slug) > max_length:
-                base_slug = base_slug[:max_length]
-
-            self.slug = base_slug
+            self.slug = self.get_safe_slug(self.name)
 
         super().save(*args, **kwargs)
